4925faa90c5440000f5eb9c958dfca5daf4ef999
naga <pujolchr@gmail.com>
2017-02-16 14:00:24 +0100

    create js file for basic ptototype

---
+++ proto.js
@@ -0,0 +1,18 @@
+//prototype.js
+// pujolchr AT gmiail.com
+
+
+var Paper = {
+    name;
+    url;
+    date-in;
+}
+
+var stack = [];
+
+function pushToRead(paper) {}
+
+
+function appendToRead(paper) {}
+
+function popToRead(paper) {}
