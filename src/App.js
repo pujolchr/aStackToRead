5114c268c4ac0400ec210b7f5dd38185f00dacfb
Christian Svendsen Pujol <pujolchr@gmail.com>
2017-02-19 17:21:29 +0100

    First structure for the elemts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+    Component
+} from 'react';
 import './App.css';
+import stack from '../public/stack.json';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class Control extends Component {
+    render() {
+        return ( <div>
+            <button>add in top</button>
+            <button>add in bottom</button>
+            <button>read</button>
+            </div>
+        );
+    }
 }
 
+class FirstInLine extends Component {
+
+    render() {
+        return (
+                <h2>first</h2>
+               );
+    }
+}
+
+class NextInLine extends Component {
+    render() {
+        return (
+                <ul>
+                    Line
+                </ul>
+               );
+    }
+}
+class Stack extends Component {
+    constructor(props) {
+            super(props);
+            this.state = {"stack": stack}
+            }
+
+    render() {
+        return (
+                <div>
+                    <FirstInLine />
+                    <NextInLine />
+                    ldfkjsl
+                </div>
+                );
+    }
+}
+class App extends Component {
+    render() {
+        return ( <div>
+            <Control />
+            <Stack />
+            </div>
+        );
+    }
+}
+
+console.log(stack);
 export default App;
